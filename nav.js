79226d9e3ea7324e57607490397e69340bb91c26
dhanvanthgs25@gmail.com <dhanvanthgs25@gmail.com>
2025-05-16 21:39:53 +0530

    profile button modified

--- nav.js
+++ nav.js
@@ -10,13 +10,16 @@ fetch("http://localhost:5000/profile",{
         const prof = document.getElementById('prof');
         prof.setAttribute('href', 'profile.html');
         prof.innerHTML = ''; 
+        const new_button=document.createElement('button');
+        new_button.setAttribute('id','prof');
         const img = document.createElement('img');
-        img.setAttribute('src', 'https://i.imgur.com/6VBx3io.png');
+        img.setAttribute('src', 'profilepic-removebg-preview.png');
         img.setAttribute('alt', 'user pic');
-        img.setAttribute('width', '40'); 
-        img.setAttribute('height', '40');
+        img.setAttribute('width', '80'); 
+        img.setAttribute('height', '80');
         img.style.borderRadius = '50%'; 
         img.style.objectFit = 'cover'; 
-        prof.appendChild(img);
+        new_button.appendChild(img);
+        prof.replaceChild(new_button);
     }    
 })
